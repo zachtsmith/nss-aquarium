03293061bbc2763d242c8ad979424b055899cd46
Zachary Smith <zach2993@gmail.com>
2022-06-08 11:02:51 -0500

    added database to prevent harcoding fish, also added FishList to pull data from database

---
+++ scripts/database.js
@@ -0,0 +1,29 @@
+const database = {
+        fish: [
+        {
+            id: 1,
+            species: "Rainbow Trout",
+            length: "4 inches",
+            nickname: "rainy",
+            where it was harvested: "Mississippi River"
+        }
+    ]
+        fish: [
+        {
+            id: 2,
+            species: "Big Snapper",
+            length: 36,
+            nickname: "snap snap"
+                where it was harvested: "Australlia"
+        }
+    ]
+        fish: [
+        {
+            id: 3,
+            species: "Sea Turtle",
+            length: 12,
+            nickname: "Riptide",
+            where it was harvested: "Hawaii"
+        }
+    ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// List of Requirements/ Pages
+// -Current Fish Owned
+//    On list of fish page- have an aside (info to put care of fish and tank)
+//      details of each Fish include: image, species, length, nickname, where it was harvested, and what it eats
+
+// -Where You Harvested Fish
+
+
+// -On the Harvest page, provide travel quotes for people, listed above the travel locations
+
+// -Nav bar mentioned, not necessary currently.
+
+//  process of keeping the tank clean and the water at the right salinity and temperature
